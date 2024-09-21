6bfce445345c4bcd209855ab43bb880786a861c1
taykim01 <taykim01@gmail.com>
2024-09-21 16:49:37 +0900

    config: supabase config

---
+++ src/data/infrastructures/supabase/get_user.ts
@@ -0,0 +1,11 @@
+"use server";
+
+import { createClient } from "./server";
+
+export default async function getUserID() {
+    const database = createClient();
+    const {
+        data: { user },
+    } = await database.auth.getUser();
+    return user?.id;
+}
---
+++ src/data/infrastructures/supabase/tables.ts
@@ -0,0 +1,9 @@
+const TABLES = {
+    PROFILE: "PROFILE",
+    PROJECT: "PROJECT",
+    NODE: "NODE",
+    CODE: "CODE",
+    LLM_RESPNSE: "LLM_RESPONSE",
+};
+
+export default TABLES;
