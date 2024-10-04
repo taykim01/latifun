f9b53a6db76c285e166c2397053b81d6d070a1ad
ysung327 <ysung327@naver.com>
2024-10-05 01:13:08 +0900

    ADD: create empty project

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,3 +1,9 @@
+import createEmptyProjectUseCase from "@/application/use_cases/create_empty_project.use_case";
+
 export default function Home() {
-  return <></>;
+    return (
+        <form>
+            <button formAction={createEmptyProjectUseCase}>가즈아아아</button>;
+        </form>
+    );
 }
--- src/application/use_cases/customize_components.use_case.ts
+++ src/application/use_cases/customize_components.use_case.ts
@@ -5,3 +5,5 @@ export default function customizeComponentsUseCase() {
     // 색상 조합, 라운드, 그림자, 폰트 등 -> 유저가 선택 -> node.type === style에 저장
     // code tailwind.config.ts 코드 수정
 }
+
+// 주환
