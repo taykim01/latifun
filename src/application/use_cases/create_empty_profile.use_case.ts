1408a55f4825ab25bda25ed78e29488edd31f808
taykim01 <taykim01@gmail.com>
2024-10-04 20:48:39 +0900

    feat: createEmptyProfileUseCase 구현

--- src/application/use_cases/create_empty_profile.use_case.ts
+++ src/application/use_cases/create_empty_profile.use_case.ts
@@ -1,8 +1,26 @@
 // 유저 프로필 생성하는 use case
 "use server";
 
-export default function createEmptyProfileUseCase() {
+import { createClient } from "@/data/infrastructures/supabase/server";
+import TABLES from "@/data/infrastructures/supabase/tables";
+import { Tables } from "../dao/database.types";
+
+async function createProfile(profileData: Omit<Tables<"profile">, "id">) {
+    const supabase = createClient();
+    const { data, error } = await supabase.from(TABLES.PROFILE).insert(profileData).select("id")
+    if(error) throw new Error(error.message);
+    return data;
+}
+
+export default async function createEmptyProfileUseCase() {
     // 비어있는 유저 프로필 생성
+    const emptyProfile: Omit<Tables<"profile">, "id"> = {
+        created_at: new Date().toISOString(),
+          email: "",
+          name: "",
+          profile_img: null
+    }
+    await createProfile(emptyProfile)
 }
 
 // 태은
